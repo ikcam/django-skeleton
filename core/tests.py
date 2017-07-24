ea1b2c9f86bdb3626e0092671f75dacc718b0833
Irving Kcam <me@ikcam.com>
2017-07-23 23:00:35 -0500

    Fix: Tests
    User was required.

--- core/tests.py
+++ core/tests.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import AnonymousUser
 from django.test import RequestFactory, TestCase
 from . import views
 
 
 class CoreTests(TestCase):
-    def test_views_index(self):
+    def test_views_index_no_redirect(self):
         factory = RequestFactory()
         request = factory.get('/fake-path')
+        request.user = AnonymousUser()
         response = views.Index.as_view()(request)
         self.assertEqual(response.status_code, 200)
