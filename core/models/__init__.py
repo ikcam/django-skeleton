94b9e59424b3a801533232813776ba53c373aaab
Irving <me@ikcam.com>
2018-08-31 10:47:56 -0500

    Complain with Django

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,20 +1,27 @@
 from django.conf import settings
+
 import culqipy
+
+from core.models.attachment import Attachment
+from core.models.colaborator import Colaborator
+from core.models.company import Company
+from core.models.event import Event
+from core.models.invite import Invite
+from core.models.invoice import Invoice
+from core.models.link import Link
+from core.models.message import Message
+from core.models.notification import Notification
+from core.models.payment import Payment
+from core.models.role import Role
+from core.models.visit import Visit
+from core.models.user import User
 
 
 culqipy.public_key = settings.CULQI_PUBLIC_KEY
 culqipy.secret_key = settings.CULQI_PRIVATE_KEY
 
-from .attachment import Attachment  # NOQA
-from .colaborator import Colaborator  # NOQA
-from .company import Company  # NOQA
-from .event import Event  # NOQA
-from .invite import Invite  # NOQA
-from .invoice import Invoice  # NOQA
-from .link import Link  # NOQA
-from .message import Message  # NOQA
-from .notification import Notification  # NOQA
-from .payment import Payment  # NOQA
-from .role import Role  # NOQA
-from .visit import Visit  # NOQA
-from .user import User  # NOQA
+
+__all__ = [
+    'Attachment', 'Colaborator', 'Company', 'Event', 'Invite', 'Invoice',
+    'Link', 'Message', 'Notification', 'Payment', 'Role', 'Visit', 'User'
+]
